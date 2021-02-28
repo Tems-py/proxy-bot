6c551e8c893014587ea9dca993b2657cb14c0c48
Tems <79542785+Tems-py@users.noreply.github.com>
2021-02-28 11:39:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ client = commands.Bot(command_prefix="-", help_command=None)
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game('Proxy bot by Tems'))
     print("Bot ready")
 
         
         
 @client.command()
-async def proxy(ctx):
+async def proxy(ctx, arg1 = None):
+    if arg1 == None:
+        em = discord.Embed(Title="Usage", description="-proxy [http / socks4 / socks5]")
+        await ctx.send(embed=em)
+        return
     scraped = 0
     f = open("proxies.txt", "a+")
     f.truncate(0)
-    r = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1500&ssl=yes')
+    r = requests.get(f'https://api.proxyscrape.com/?request=displayproxies&proxytype={arg1}&timeout=1500&ssl=yes')
     proxies = []
     for proxy in r.text.split('\n'):
         proxy = proxy.strip()
@@ -30,5 +35,5 @@ async def proxy(ctx):
     
     
     
-    
+#This code were made by Tems
 client.run('TOKEN')
